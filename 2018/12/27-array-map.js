bdcd61250358739522801c2c951924938b36dd78
Varvara Kronberg <github@varvara.ca>
2018-12-27 01:07:11 -0800

    callbacks and array.map

---
+++ 2018/12/27-array-map.js
@@ -0,0 +1,11 @@
+/**
+ * Write a program that prints all elements of the array multiplied by 2, without using loops.
+ * Hint: Google js array map
+ */
+
+var array = [1, 2, 3];
+
+const map1 = array.map(i => i * 2);
+
+console.log(map1);
+
---
+++ 2018/12/27-callbacks.js
@@ -0,0 +1,26 @@
+/**
+ * 1. Write a function that asks user for a password, and if it's correct it prints "CORRECT!", otherwise it prints "INCORRECT!"
+ * 2. Change the code so that there's no console.log calls in the checking function itself.
+ */
+var readLine = require('readline-sync');
+
+passwordCheck(correct, incorrect);
+
+function correct() {
+    console.log("CORRECT");
+}
+
+function incorrect() {
+    passwordCheck(correct, incorrect);
+}
+
+
+function passwordCheck(successCallback, failureCallback) {
+    var userInput = readLine.question('password:');
+    if (userInput != 'password') {
+        failureCallback();
+    }
+    else {
+        successCallback()
+    }
+}
